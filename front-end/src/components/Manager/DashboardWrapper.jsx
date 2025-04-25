c9d30b83948496d175f15508a9efa8760c41cfb8
Minh ha <haminh0423@gmail.com>
2025-04-25 15:27:24 +0700

    fix bug

--- front-end/src/components/Manager/DashboardWrapper.jsx
+++
@@ -1,9 +0,0 @@
-
-const DashboardWrapper = ({ children }) => {
-    return (
-        <div className="container-xxl position-relative bg-white d-flex p-0">
-            {children}
-        </div>
-    );
-};
-export default DashboardWrapper;
